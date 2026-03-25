6fcf1f995eb2fc9fca401f2e18f9f306afd85522
isavmp1 <isabela.raposeiro@escola.pr.gov.br>
2026-03-25 12:02:15 +0000

    versao2

---
+++ produto.js
@@ -0,0 +1,8 @@
+const produto = "Mochila Impermeável";
+let preco = 189.90;
+let estoque = 14;
+const categoria = "acessorios";
+
+console.log(`A ${produto} causa R$${preco}.`);
+console.log(`Temos ${estoque} itens no estoque.`);
+console.log(`O produto pertence a categoria ${categoria}.`);
