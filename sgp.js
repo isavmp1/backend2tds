b0d32684b6a1968f672b3a17cbcf5f841abd92d7
isavmp1 <isabela.raposeiro@escola.pr.gov.br>
2026-04-29 13:54:45 +0000

    versao2

--- sgp.js
+++ sgp.js
@@ -16,6 +16,24 @@ function filtrarMissoesConcluidas(listaDeMissoes) {
         return foiFinalizada;
     };
 
+
     const apenasConcluidas = listasDeMissoes.filter(verificarStatusDaMissao);
     return apenasConcluidas
 };
+
+
+IARA VITORIA FERREIRA
+10:53 (há 0 minuto)
+para mim
+
+function atualizarInventario(inventarioAtual) {
+    let novoInventario;
+    if(acao === "pegar"){
+        const inventarioComItemNovo = [inventarioAtual, NomeDoItem];
+        novoInventario = inventarioComItemNovo;
+
+    }else if (acao === descartar) // parei aqui
+}
+
+const pontosDoJogador =[100,50,200,10]
+console.log(pontosDoJogador);
