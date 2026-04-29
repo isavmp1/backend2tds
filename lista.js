ae26f2329070378c4a21a2660a2c27eb3f196d9f
isavmp1 <isabela.raposeiro@escola.pr.gov.br>
2026-04-29 13:45:45 +0000

    commit

---
+++ lista.js
@@ -0,0 +1,13 @@
+const alunos =["Isa","Clara","Damon"];
+
+alunos.push("Diego");
+alunos.unshift ("Lopes");
+alunos.unshift("Salvatore");
+console.log(alunos);
+
+console.log(alunos.indexOf("Diego"));
+console.log(alunos.indexOf("Paula"));
+console.log(alunos.indexOf("Salvatore"));
+
+console.log(alunos.includes("Elena"));
+console.log(alunos.includes("Arthur"));
