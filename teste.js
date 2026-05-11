bbeb16b83d95d91ead5a2cf46e3c00fd58509b65
isavmp1 <isabela.raposeiro@escola.pr.gov.br>
2026-05-11 13:56:55 +0000

    v1

---
+++ teste.js
@@ -0,0 +1,15 @@
+const destino = "nova york";
+const nomeViajante = "Isa";
+const cidadeOrigem = "Curitiba";
+let duracao = "12";
+let transporte = "aviao";
+
+
+console.log(`A viajante ${nomeViajante} deseja viajar de ${cidadeOrigem} até ${destino}, de ${transporte} em um
+percurso de ${duracao} horas.`);
+
+let hospedagem = 800;
+let alimentacao = 600;
+
+let total = hospedagem + alimentacao;
+console.log("O total do gastos será: R$ " + total + "reais.");
