872b22fe9bf117566ff50a63585f3378745c563f
isavmp1 <isabela.raposeiro@escola.pr.gov.br>
2026-05-13 13:56:18 +0000

    v1

---
+++ desafio.js
@@ -0,0 +1,11 @@
+//Declarar 5 variaveis para representar um funcionário
+//fazer uma saída de forma interpolada(`${}`)
+//Crie duas variaveis numericas e exiba uma soma ou multiplicação
+
+let nome = "Arthur";
+let idade = 19;
+let cargo = "Atendente";
+let salario = 1800;
+let folga = "escala 12 por 36";
+
+console.log(`O funcionário ${nome}, de ${idade} anos, trabalha como ${cargo} na folga de ${folga} e recebe R$ ${salario}.`);
