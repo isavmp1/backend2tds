59e41e6bcf239f33d16ed7c08dbb7af7b5ab2c11
isavmp1 <isabela.raposeiro@escola.pr.gov.br>
2026-05-18 13:54:32 +0000

    v2

--- desafio.js
+++ desafio.js
@@ -9,3 +9,13 @@ let salario = 1800;
 let folga = "escala 12 por 36";
 
 console.log(`O funcionário ${nome}, de ${idade} anos, trabalha como ${cargo} na folga de ${folga} e recebe R$ ${salario}.`);
+
+// Duas variáveis numéricas
+let numero1 = 10;
+let numero2 = 5;
+
+// Exibindo a soma
+console.log(`A soma de ${numero1} + ${numero2} é ${numero1 + numero2}.`);
+
+// Exibindo a multiplicação
+console.log(`A multiplicação de ${numero1} * ${numero2} é ${numero1 * numero2}.`);
