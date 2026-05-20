a7df180864a60250cdf4d2a60e1f0e25eb565e74
isavmp1 <isabela.raposeiro@escola.pr.gov.br>
2026-05-20 13:53:38 +0000

    v3

--- desafio.js
+++ desafio.js
@@ -1,6 +1,8 @@
 //Declarar 5 variaveis para representar um funcionário
 //fazer uma saída de forma interpolada(`${}`)
 //Crie duas variaveis numericas e exiba uma soma ou multiplicação
+//Declarar salario base e bonus. Calcular salário total e desconto de 10%
+//A partir da declaração de anosEmpresa e salário, exiba se recebe promoção, se esta 
 
 let nome = "Arthur";
 let idade = 19;
@@ -19,3 +21,11 @@ console.log(`A soma de ${numero1} + ${numero2} é ${numero1 + numero2}.`);
 
 // Exibindo a multiplicação
 console.log(`A multiplicação de ${numero1} * ${numero2} é ${numero1 * numero2}.`);
+
+let salarioBase = 1800;
+let bonus = 500;
+
+let salarioTotal = salarioBase + bonus;
+let salarioFinal = salarioTotal - (salarioTotal * 0.10);
+
+console.log(salarioFinal);
