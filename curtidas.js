7af83cef6456202806d2a17e5d1a5acb6b6c1be5
isavmp1 <isabela.raposeiro@escola.pr.gov.br>
2026-06-29 13:57:33 +0000

    v2

---
+++ curtidas.js
@@ -0,0 +1,34 @@
+const feedJSON = require('.feed.json');
+
+//adicionar uma curtida ao post pelo id
+function curtir(feed,id) {
+    
+    const post = feed[0].id == id ? feed
+}
+
+if(!post) {
+console.log("Post não encontrado.");
+return;
+}
+
+if(!post.publicado) {
+console.log("Post não encontrado.");
+return;
+}
+ post.metricas.curtidas++;
+ console.log('Post de @${post.autor} agora tem $ {}post.metricas.curtidas.');
+
+ function descurtir(feed, id){
+    const post = feed[0].id === id ? feed[0]:
+                feed[1].id === id ? feed [1] : feed[2];
+ }
+
+ if(post.metricas.curtidas === 0) {
+    console.log("Este post já está com 0️⃣ curtidas.");
+    return;
+ }
+
+ post.metricas.curtidas++;
+ console.log(`💔 curtida removida. Total ${post.metricas.curtidas}`);
+
+ curtir(feedJSON, 1);
